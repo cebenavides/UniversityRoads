b4a7696ff51179a3c10cbf860fb1ac98e701cdab
djmv <dayanm@uninorte.edu.co>
2017-04-12 16:08:42 -0500

    Último caso

--- UniversityRoads/src/com/university/UniversityRoads.java
+++ UniversityRoads/src/com/university/UniversityRoads.java
@@ -29,14 +29,14 @@ public class UniversityRoads {
 
         //Ubicación de las personas y las cajas
         people.add(new Element("Jorge", "A"));
-        people.add(new Element("Mike", "E"));
-        people.add(new Element("Dayo2", "C"));
-        boxes.add(new Element("Caja 1", "B"));
+        people.add(new Element("Dayan", "E"));
+        people.add(new Element("Mike", "B"));
+        boxes.add(new Element("Caja 1", "C"));
     //    boxes.add(new Element("Caja 1", "E"));
         
        
         boxes.add(new Element("Caja 2", "D"));
-        boxes.add(new Element("Caja 3", "G"));
+//        boxes.add(new Element("Caja 3", "G"));
 //        boxes.add(new Element("Caja 4", "C"));
 
         /*Inicialización de la lista en la que quedarán las rutas que deberá
@@ -68,21 +68,29 @@ public class UniversityRoads {
             //Utilities.printMatrix(shortest);
             System.out.println(rdist.toString());
             
+            
             for (int i = 0; i < rdist.size(); i++) {
                 rdist.get(i).sort(Comparator.comparing(Distance::distance));
                 //rdist.sort((o1, o2) -> o1.distance().compareTo(o2.distance()));
             }
             System.out.println(rdist.toString());
             List<Integer> destination = new ArrayList<>();
+            List<String> desti = new ArrayList<>();
+            List<Double> camino = new ArrayList<>();
+            List<Integer> copydestine = new ArrayList<>();
             List<Integer> indices = new ArrayList<>();
             for (int i = 0; i < rdist.size(); i++) {
                 destination.add(rdist.get(i).get(0).location);
-                indices.add(0);                
+                indices.add(0); 
+                desti.add(rdist.get(i).get(0).word);
+                camino.add(0.0);
             }
             System.out.println("Destinos: " + destination.toString());
             int k=4;
             int c=0;
+
             for (int i = 0; i < destination.size()-1; i++) {
+                reinicio:
                 for (int j = i+1; j < destination.size(); j++) {
                     
                     if (destination.get(i) == destination.get(j)) {
@@ -98,22 +106,30 @@ public class UniversityRoads {
                             
                                 if (dist11.distance > dist22.distance) {
                                     destination.set(j, dist22.location);
+                                    desti.set(j,dist22.word);
                                     indices.set(j, indices.get(j) + 1);
                                     k=0;
+                                    i=-1;
+                                    break reinicio;
                                 }else{
                                     destination.set(i, dist11.location);
                                     indices.set(i, indices.get(i) + 1);
+                                    desti.set(i,dist11.word);
                                     k=1;
+                                    i=-1;
+                                    break reinicio;
                                 }
                             
                             }else if(dist1.distance > dist2.distance){
                                 if((indices.get(i)+1)<boxes.size()){
                                 Distance dist112 = rdist.get(i).get(indices.get(i)+1);    
                                 destination.set(i, dist112.location);
+                                desti.set(i,dist112.word);
                                 indices.set(i, indices.get(i)+1);
                                 }
                                 else{                                  
                                   peopleDP.add(people.get(i));
+                                  desti.set(i,"DP");
                           //        people.remove(i);
                                 System.out.println("Hola mundo cruel"); 
                                 
@@ -125,23 +141,26 @@ public class UniversityRoads {
                                 if((indices.get(j)+1)<boxes.size()){
                                 Distance dist222 = rdist.get(j).get(indices.get(j)+1);    
                                 destination.set(j, dist222.location);
+                                desti.set(j,dist222.word);
                                 indices.set(j, indices.get(j)+1);                                
                                 }
                                 else{                                   
                                   peopleDP.add(people.get(j));
+                                  desti.set(j,"DP");
                                   //people.remove(j);
                                 System.out.println("Hola mundo cruel"); 
                                 
                                 }
                             }else{
                                 peopleDP.add(people.get(j));
+                                desti.set(j,"DP");
                             }
                     }                      
                 }
             }
         
             System.out.println("Destinos nuevos: " + destination.toString());
-            
+            System.out.println("Desti nuevos: " + desti.toString());
             if (people.size()<=boxes.size()){
                 System.out.println("PERSONAS <= CAJAS");
                 for (int i = 0; i < people.size(); i++) {
@@ -180,20 +199,33 @@ public class UniversityRoads {
                             = fw.getPath(people.get(i).location, boxes.get(destination.get(i)).location);
                         routes.get(i).add(path);
                         people.get(i).location = boxes.get(destination.get(i)).location; 
-                             caso = destination.get(i);
-                            boxes.remove(caso);       
+                             copydestine.add(destination.get(i));   
                             path = fw.getPath(people.get(i).location, "DP");
                             routes.get(i).add(path);
                             people.get(i).location = "DP";    
                          }
                      }
-                }                
+                }  
+                Collections.sort(copydestine, Collections.reverseOrder());
+                for (int i : copydestine) {
+                    boxes.remove(i);
+                }
             }
         }
+            List<Double> camino = new ArrayList<>();
+            for (int i = 0; i < routes.size(); i++) {
+                camino.add(0.0);
+            }
         System.out.println("\nRESULTADOS");
+        double c;
         for (int i = 0; i < routes.size(); i++) {
             System.out.println("Ruta que tomará " + people.get(i).name);
             System.out.println(routes.get(i).toString());
+            for (int j = 0; j < routes.get(i).size(); j++) {
+                c=routes.get(i).get(j).getWeight();
+                camino.set(i,c+camino.get(i));
+            }
+            System.out.println(camino.get(i).toString());
         }
 
     }
@@ -219,7 +251,11 @@ public class UniversityRoads {
         g.addVertex("B");
         g.addVertex("C");
         g.addVertex("D");
-
+        g.addVertex("DP");
+        g.addVertex("E");
+        g.addVertex("F");
+        g.addVertex("G");
+/*
         g.addVertex("E");
         g.addVertex("F");
         g.addVertex("G");
@@ -227,19 +263,15 @@ public class UniversityRoads {
         g.addVertex("K");
         g.addVertex("H");
         g.addVertex("DP");
-          
+  */        
         //Añadiendo las aristas (rutas entre los lugares) con sus pesos
-        g.setEdgeWeight(g.addEdge("A", "B"), 1);
-        g.setEdgeWeight(g.addEdge("B", "C"), 1);
-        g.setEdgeWeight(g.addEdge("C", "D"), 1);
-        
-        g.setEdgeWeight(g.addEdge("D", "E"), 1);
-        g.setEdgeWeight(g.addEdge("E", "F"), 1);
-        g.setEdgeWeight(g.addEdge("F", "G"), 1);
-        g.setEdgeWeight(g.addEdge("G", "H"), 1);
-        g.setEdgeWeight(g.addEdge("H", "I"), 1);
-        g.setEdgeWeight(g.addEdge("I", "A"), 1);
-        g.setEdgeWeight(g.addEdge("A", "DP"), 1);
+        g.setEdgeWeight(g.addEdge("A", "B"), 1.1);
+        g.setEdgeWeight(g.addEdge("A", "C"), 1.2);
+        g.setEdgeWeight(g.addEdge("A", "DP"), 10);
+        g.setEdgeWeight(g.addEdge("B", "D"), 1.3);
+        g.setEdgeWeight(g.addEdge("B", "C"), 1.4);
+        g.setEdgeWeight(g.addEdge("C", "E"), 1.5);
+        g.setEdgeWeight(g.addEdge("D", "E"), 2);
 
         return g;
     }
